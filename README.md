775bad4a2dd30172760577483672279da771ebf0
gurge3 <wu.x@husky.neu.edu>
2017-05-14 17:31:53 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+I am Huisiyu Yu. Now I am a sophmore in NEU.
+
+This is for web development summer 1 2017 assignments and the final project.
+
+Heroku: https://yu-huisiyu-webdev.herokuapp.com
---
+++ test/app.js
@@ -0,0 +1,68 @@
+module.exports = function(app)
+{
+    app.get("/api/test", findAllMessages);
+    app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);
+
+    // Used temporarily for accessing MLab MongoDB.
+    var username = "admin";
+    var password = "admin";
+
+    var connectionString = 'mongodb://'+ username + ':' + password + '@ds113628.mlab.com:13628/heroku_9bxp0pq5';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+
+    var TestSchema = mongoose.Schema({
+        message: String
+    });
+
+    var TestModel = mongoose.model("TestModel", TestSchema);
+
+    function findAllMessages(req, res) {
+        TestModel
+            .find()
+            .then(
+                function(tests) {
+                    res.json(tests);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function createMessage(req, res) {
+        TestModel
+            .create(req.body)
+            .then(
+                function(test) {
+                    res.json(test);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteMessage(req, res) {
+        TestModel
+            .remove({_id: req.params.id})
+            .then(
+                function(result) {
+                    res.json(result);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+};
