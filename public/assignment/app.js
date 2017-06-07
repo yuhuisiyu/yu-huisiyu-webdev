52edcd9f62c848f3994a525cad669084372ea97c
yuhuisiyu <yu.huis@husky.neu.edu>
2017-06-06 23:55:12 -0400

    Assignment4

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,4 +1,7 @@
-(function () {
-    angular
-        .module('WebAapMaker', ['ngRoute']);
-})();
+module.exports = function(app) {
+    require("./services/user.service.server.js")(app);
+    require("./services/page.service.server.js")(app);
+    require("./services/website.service.server.js")(app);
+    require("./services/widget.service.server.js")(app);
+};
+
