f4f740a37cc7a17cb3b0d672233d488a581578b4
yuhuisiyu <yu.huis@husky.neu.edu>
2017-06-08 16:56:56 -0400

    flickr

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -70,5 +70,11 @@
                 controller: 'widgetEditController',
                 controllerAs: 'model'
             })
+            .when('/user/:uid/website/:wid/page/:pid/widget/:wgid/search', {
+                templateUrl: 'views/widget/templates/widget-flickr-search.view.client.html',
+                controller: 'flickrSearchController',
+                controllerAs: 'model'
+            })
+
     }
 })();
