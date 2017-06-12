dcdd9391e9adca531e1d7a8413e5e0ee08a03e3d
yuhuisiyu <yu.huis@husky.neu.edu>
2017-06-12 12:52:07 -0400

    Hahaha

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,4 +1,6 @@
 module.exports = function(app) {
+    var mongoose = require("mongoose");
+
     require("./services/user.service.server.js")(app);
     require("./services/page.service.server.js")(app);
     require("./services/website.service.server.js")(app);
