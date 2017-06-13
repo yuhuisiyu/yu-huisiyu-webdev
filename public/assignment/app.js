3f4a1a61131f419558cf6e13234b08bc40ee27ca
yuhuisiyu <yu.huis@husky.neu.edu>
2017-06-13 09:27:43 -0400

    Assignment 5 updated server

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,8 +3,24 @@ module.exports = function(app) {
     mongoose.Promise = require('q').Promise;
 
 
-    mongoose.connect("mongodb://127.0.0.1/yuhuisiyu-webdev");
+    // mongoose.connect("mongodb://127.0.0.1/yuhuisiyu-webdev");
 
+    // Used temporarily for accessing MLab MongoDB.
+    var username = "admin";
+    var password = "admin";
+
+    var connectionString = 'mongodb://'+ username + ':' + password + '@ds143221.mlab.com:43221/heroku_dgt90mmz';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
 
 
     require("./services/user.service.server.js")(app);
